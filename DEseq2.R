cb1c4086c6a06b2487caac9a3257d3d254390b0c
Donovin Coles <148016148+Coles-DW@users.noreply.github.com>
2024-11-20 13:48:06 +1100

    Update and rename DEseq2_MR.R to DEseq2.R

--- DEseq2.R
+++ DEseq2.R
@@ -10,34 +10,29 @@ BiocManager::install("ggplot2")
 library("DESeq2")
 library("ggplot2")
 library('reshape')
-workingDir = "C:/Users/30059564/OneDrive - Western Sydney University/Myrtle rust/Apsidii control paper/Plant RNA-seq/filtering counts/deseq2"
+workingDir = "##location here"
 setwd(workingDir)
-data=read.table("apsidii counts_filtered.txt", header = TRUE, row.names = 1)
+data=read.table("file.txt", header = TRUE, row.names = 1)
 colnames(data)
 sample=colnames(data)
-#condition = c("ContS","ContS","ContS","InocS","InocS","InocS","HypS","HypS","HypS",
-              #"InocHypS","InocHypS","InocHypS","ContR","ContR","ContR","InocR","InocR","InocR")
-
-condition = c("ContS","ContS","ContS","HypS","HypS","HypS","ContR","ContR","ContR")
-
-condition = c("InocS","InocS","InocS","InocHypS","InocHypS","InocHypS","InocR","InocR","InocR")
+#condition = c("InocS","InocS","InocS","InocHypS","InocHypS","InocHypS","InocR","InocR","InocR")
 condition = c("Myrtle_Rust_Susceptible","Myrtle_Rust_Susceptible","Myrtle_Rust_Susceptible","Myrtle_Rust_Hyperparasite_Susceptible","Myrtle_Rust_Hyperparasite_Susceptible","Myrtle_Rust_Hyperparasite_Susceptible","Myrtle_Rust_Resistant","Myrtle_Rust_Resistant","Myrtle_Rust_Resistant")
 colData=cbind(colnames(data), sample, condition)
 ######verification des col -treatment###
 colData=data.frame(colData)
-write.table(colData, file="MRcol_Samples_Conditions_myrtle.txt", sep="\t")
+write.table(colData, file="col_Samples_Conditions.txt", sep="\t")
 ######boxplot of data transnformation
 epsilon <- 1 # pseudo-count to avoid problems with log(0)
 hist(as.matrix(log2(data + epsilon)), breaks=100, col="blue", border="white",
      main="Expressed_Log2-transformed counts per gene_myrtle", xlab="log2(counts+1)", ylab="Number of genes", 
      las=1, cex.axis=0.7)
-dev.copy(png,'Log2-transformed_Histogram_myrtle.png')
+dev.copy(png,'Log2-transformed_Histogram.png')
 dev.off()
 condition_1 <- as.data.frame(t(condition))
 boxplot(log2(data + epsilon), condition_1$color, pch=".", 
         horizontal=TRUE, cex.axis=0.5,
         las=1, ylab="Samples", xlab="log2(counts +1)")
-dev.copy(png,'boxplot_expressed_transformeddata_myrtle.png')
+dev.copy(png,'boxplot_expressed_transformeddata.png')
 dev.off()
 ######dds=data deseq
 dds= DESeqDataSetFromMatrix(data, colData, ~condition)
@@ -46,7 +41,7 @@ results(dds)
 str(dds)
 ######sortie donnees normalises############
 norm_counts=counts(dds, normalized=TRUE)
-write.table(norm_counts, file="myrtle_normalized_expressed.txt", sep="\t")
+write.table(norm_counts, file="normalized_expressed.txt", sep="\t")
 ##rlog tranformation
 rld<- rlogTransformation(dds, blind=TRUE)
 distsRL <- dist(t(assay(rld)))
@@ -60,7 +55,7 @@ library("gplots")
 library('RColorBrewer')
 hmcol <- colorRampPalette(brewer.pal(9, "GnBu"))(100)
 heatmap.2(mat, trace="none", col = rev(hmcol), margin=c(10, 10))
-dev.copy(png,'Expressed_myrtle_sampletosample_heatmap.png')
+dev.copy(png,'Expressed_sampletosample_heatmap.png')
 dev.off()
 pca_1<-print(plotPCA(rld))
 pcaData <- plotPCA(rld, intgroup = c("condition"), returnData = TRUE)
@@ -77,7 +72,7 @@ ggplot(pcaData, aes(PC1, PC2, color=condition)) +
   xlab(paste0("\n PC1: ",percentVar[1],"% variance")) +
   ylab(paste0("PC2: ",percentVar[2],"% variance \n")) + 
   theme_classic()
-dev.copy(png, 'priming_PCA.png')
+dev.copy(png, 'PCA.png')
 dev.off()
 #####savoir la dimension du fichier, nombre col et lignes)###
 dim(norm_counts)
@@ -88,25 +83,24 @@ str(dds)
 norm_counts_log=log2(norm_counts+1)
 ## calcul correlation de Pearson##
 r = cor(norm_counts_log, method = "pearson")
-write.table(r, file="myrtle_correlation_values.txt", sep="\t")
+write.table(r, file="correlation_values.txt", sep="\t")
 ## calcul de la dissimilarité ##
 d=1-r
 ## Clustering ##
 h = hclust(as.dist(d), method = "complete", members = NULL)
 plot(h)
-dev.copy(pdf, 'Expressed_myrtle_hclust.pdf')
+dev.copy(pdf, 'Expressed_hclust.pdf')
 dev.off()
 
 ####comparaison####
 ####Pairwise comparison#####
-res1 <- results(dds, contrast=c("condition","ContR","ContS"))
-res2 <- results(dds, contrast=c("condition","HypS","ContS"))
 
 res1 <- results(dds, contrast=c("condition","InocR","InocS"))
 res2 <- results(dds, contrast=c("condition","InocHypS","InocS"))
 
 res1 <- results(dds, contrast=c("condition","Myrtle_Rust_Resistant","Myrtle_Rust_Susceptible"))
 res2 <- results(dds, contrast=c("condition","Myrtle_Rust_Hyperparasite_Susceptible","Myrtle_Rust_Susceptible"))
+
 ######Mise en forme#########
 res1=res1[,c(2,6)]
 res2=res2[,c(2,6)]
@@ -119,6 +113,6 @@ colnames(res2)=c("Log2Foldchange_InocHypSvsInocS","padj_InocHypSvsInocS")
 colnames(res1)=c("Log2Foldchange_Myrtle_Rust_ResistantvsMyrtle_Rust_Susceptible","padj_Myrtle_Rust_ResistantvsMyrtle_Rust_Susceptible")
 colnames(res2)=c("Log2Foldchange_Myrtle_Rust_Hyperparasite_SusceptiblevsMyrtle_Rust_Susceptible","padj_Myrtle_Rust_Hyperparasite_SusceptiblevsMyrtle_Rust_Susceptible")
 
-myrtlegeneexpressionresults=cbind(res1, res2)
-write.table(myrtlegeneexpressionresults, file="Differentialexpression_myrtle pathogen.txt", sep="\t")
+geneexpressionresults=cbind(res1, res2)
+write.table(geneexpressionresults, file="Differentialexpression.txt", sep="\t")
 
